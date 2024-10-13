85439b45f54fe357e4bd611b3485faf7dc7d71ac
David Goldstein <goldstein.g.david@gmail.com>
2024-10-13 12:57:57 -0400

    Deploy Lambda artifacts using CDK

--- bundle.sh
+++ bundle.sh
@@ -11,13 +11,13 @@ echo "Generating templ files..."
 templ generate
 
 echo "Compiling handlers..."
-mkdir -p build/home build/station build/static
+mkdir -p build/home build/station build/static build/bundles
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -tags lambda.norpc -o ./build/home/bootstrap ./lambda/home/main.go
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -tags lambda.norpc -o ./build/station/bootstrap ./lambda/station/main.go
 
 echo "Bundling files..."
-zip home.zip -j ./build/home/bootstrap
-zip station.zip -j ./build/station/bootstrap
+zip build/bundles/home.zip -j ./build/home/bootstrap
+zip build/bundles/station.zip -j ./build/station/bootstrap
 
 echo "Compiling tailwindcss..."
 npx tailwindcss -i ./internal/assets/tailwind.css -o ./build/static/style.css
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -3,6 +3,9 @@ import { Construct } from "constructs";
 
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+
+const BUILD_DIR = "../build";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -11,12 +14,32 @@ export class CdkStack extends cdk.Stack {
       analyticsReporting: false, // Disable CDK Metadata
     });
 
+    const subwayPageLambda = new lambda.Function(this, "SubwayPageLambda", {
+      runtime: lambda.Runtime.PROVIDED_AL2023,
+      code: lambda.Code.fromAsset(`${BUILD_DIR}/bundles/home.zip`),
+      handler: "handler",
+    });
+    subwayPageLambda.applyRemovalPolicy(cdk.RemovalPolicy.RETAIN);
+    subwayPageLambda.role?.applyRemovalPolicy(cdk.RemovalPolicy.RETAIN);
+
+    const subwayStationLambda = new lambda.Function(
+      this,
+      "SubwayStationLambda",
+      {
+        runtime: lambda.Runtime.PROVIDED_AL2023,
+        code: lambda.Code.fromAsset(`${BUILD_DIR}/bundles/station.zip`),
+        handler: "handler",
+      },
+    );
+    subwayStationLambda.applyRemovalPolicy(cdk.RemovalPolicy.RETAIN);
+    subwayStationLambda.role?.applyRemovalPolicy(cdk.RemovalPolicy.RETAIN);
+
     const staticAssetBucket = new s3.Bucket(this, "StaticAssetBucket", {
       removalPolicy: cdk.RemovalPolicy.RETAIN,
     });
 
     new s3deploy.BucketDeployment(this, "DeployCSS", {
-      sources: [s3deploy.Source.asset("../build/static")],
+      sources: [s3deploy.Source.asset(`${BUILD_DIR}/static`)],
       destinationBucket: staticAssetBucket,
       destinationKeyPrefix: "",
     });
